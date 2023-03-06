9ba8feae0bd46b7320ee9f87eb7f5f04d9ff7c21
Gama1351 <garerimath73660@gmail.com>
2023-03-06 17:11:35 +0100

    ajout Reservation, Particpant et Participer

---
+++ APISAE401/Models/EntityFramework/Participer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APISAE401.Models.EntityFramework
+{
+    [Table("t_j_participer_ptce")]
+    public partial class Participer
+    {
+        [Key]
+        [Column("ptca_idparticipant")]
+        public int IdParticipant { get; set; }
+
+        [Key]
+        [Column("rsv_idreservation")]
+        public int IdReservation { get; set; }
+
+        [ForeignKey("IdParticipant")]
+        [InverseProperty("")]
+        public virtual Participant ParticipantNaviguation { get; set; } = null!;
+
+        [ForeignKey("IdReservation")]
+        [InverseProperty("ParticiperReservation")]
+        public virtual Reservation ReservationNavigation { get; set; } = null!;
+    }
+}
--- APISAE401/Models/EntityFramework/Reservation.cs
+++ APISAE401/Models/EntityFramework/Reservation.cs
@@ -12,7 +12,7 @@ namespace APISAE401.Models.EntityFramework
     public class Reservation
     {
         [Key]
-        [Required]
+        [Column("rsv_idreservation")]
         public int IdReservation { get; set; }
 
         [Column("rsv_datereservation", TypeName = "date")]
@@ -22,5 +22,7 @@ namespace APISAE401.Models.EntityFramework
         public decimal Montant { get; set; }
 
         // ForeignKey : idClub, IdClient, dateCal à faire
+        [InverseProperty("ReservationNavigation")]
+        public virtual ICollection<Participer> ParticiperReservation { get; set;} = new List<Participer>();
     }
 }
