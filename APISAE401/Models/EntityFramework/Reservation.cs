0dc3dc19c692151760112be4a468b0bde41cb6b0
Gama1351 <garerimath73660@gmail.com>
2023-03-06 16:43:29 +0100

    ajout Reservation

---
+++ APISAE401/Models/EntityFramework/Reservation.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Sockets;
+
+namespace APISAE401.Models.EntityFramework
+{
+    [Table("t_e_reservation_rsv")]
+    public class Reservation
+    {
+        [Key]
+        [Required]
+        public int IdReservation { get; set; }
+
+        [Column("rsv_datereservation", TypeName = "date")]
+        public DateTime DateReservation { get; set; }
+
+        [Column("rsv_montant")]
+        public decimal Montant { get; set; }
+
+        // ForeignKey : idClub, IdClient, dateCal à faire
+    }
+}
