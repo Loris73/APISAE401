2d07f1d7fe8ea026642e80e539b0ea00574309b8
JulesSs05 <jules.silvestri@etu.univ-smb.fr>
2023-03-06 17:41:04 +0100

    push typeactivite

---
+++ APISAE401/Models/EntityFramework/TypeActivite.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace APISAE401.Models.EntityFramework
+{
+    [Table("t_e_type_activite_tat")]
+    [Index(nameof(IdTypeActivite), IsUnique = true)]
+
+    public partial class TypeActivite
+    {
+        [Key]
+        [Column("tat_id")]
+        public int IdTypeActivite { get; set; }
+
+        [Required]
+        [Column("tat_nom")]
+        [StringLength(255)]
+        public string NomTypeActivite { get; set; }
+
+        [Required]
+        [Column("tat_description")]
+        public string DescriptionTypeActivite { get; set; }
+    }
+}
