18989032324b3d658e236b39d7fbfb4e2bf8a799
JulesSs05 <jules.silvestri@etu.univ-smb.fr>
2023-03-08 14:18:22 +0100

    Création de la tabel photo

---
+++ APISAE401/Models/EntityFramework/Photo.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace APISAE401.Models.EntityFramework
+{
+    /*----Jules---- => 
+    * Model Reponse
+    * Modifié le 07/03/2023 par Jules
+    */
+
+    [Table("t_e_photo_pht")]
+    [Index(nameof(IdPhoto), IsUnique = true)]
+
+    public partial class Photo
+    {
+        //=======================================
+        //Properties du model Reponse
+        [Key]
+        [Column("pht_id")]
+        public int IdPhoto { get; set; }
+
+        [Required]
+        [Column("bar_id")]
+        public int IdBar { get; set; }
+
+        [Required]
+        [Column("tat_id")]
+        public int IdTypeActivite { get; set; }
+
+        [Required]
+        [Column("rsn_id")]
+        public int IdRestaurant { get; set; }
+
+        [Required]
+        [Column("skb_id")]
+        public int IdDomaineSkiable { get; set; }
+
+        [Required]
+        [Column("clb_id")]
+        public int IdClub { get; set; }
+
+        [Required]
+        [Column("tpc_id")]
+        public int IdTypeChambre { get; set; }
+
+        [Required]
+        [Column("pht_titre")]
+        public string photo { get; set; }
+
+        //=======================================
+
+        //=======================================
+        // ForeignKeys => IdClient, IdClub, IdAvis
+
+        [ForeignKey("IdClient")]
+        [InverseProperty("ReponsesClientNavigation")]
+        public virtual Client ClientReponsesNavigation { get; set; } = new Client();
+
+        [ForeignKey("IdClub")]
+        [InverseProperty("ReponsesClubNavigation")]
+        public virtual Club ClubReponsesNavigation { get; set; } = new Club();
+
+        [ForeignKey("IdAvis")]
+        [InverseProperty("ReponsesNavigation")]
+        public virtual Avis AvisNavigation { get; set; } = new Avis();
+        //=======================================
+
+        //=======================================
+        // InverseProperty => IdClient, IdClub, IdAvis
+
+        //=======================================
+    }
+    //------------------------------
+}
